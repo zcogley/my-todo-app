a6fce3c40a4b41063862724d4e0547a57aa143b1
Zac Cogley <zaccogley@gmail.com>
2017-05-08 16:22:52 -0500

    oggi running on django

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'todo/index.html', {'posts': posts})
